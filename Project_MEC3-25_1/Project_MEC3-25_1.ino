53dd12a81b6c2e0d11e8d0a8810f5423606f8048
Wafi41 <wafins41@gmail.com>
2025-04-26 10:53:30 +0800

    Update Project_MEC3-25_1.ino

--- Project_MEC3-25_1/Project_MEC3-25_1.ino
+++ Project_MEC3-25_1/Project_MEC3-25_1.ino
@@ -1,21 +1,29 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 
-// Define Arduino pins connected to Hb808c
-const int LPUL = 2;  // Pulse pin
-const int LDIR = 3;  // Direction pin
-const int LENA = 4;  // Enable pin
+const int LPUL = 2;  // Left Pulse
+const int LDIR = 3;  // Left Direction
+const int LENA = 4;  // Left Enable
 
-const int RPUL = 5;  // Pulse pin
-const int RDIR = 6;  // Direction pin
-const int RENA = 7;  // Enable pin
+const int RPUL = 5;  // Right Pulse
+const int RDIR = 6;  // Right Direction
+const int RENA = 7;  // Right Enable
 
 ros::NodeHandle nh;
-String command = "";  // Variable to store received command
+String command = "";  
+bool shouldMove = false;  // Flag to control continuous movement
 
-// Callback function for ROS subscriber
 void commandCallback(const std_msgs::String &msg) {
   command = msg.data;
+  if (command == "F") {
+    shouldMove = true;  // Enable continuous movement
+    digitalWrite(LENA, LOW);  // Enable motors
+    digitalWrite(RENA, LOW);
+  } else if (command == "S") {
+    shouldMove = false;  // Stop movement
+    digitalWrite(LENA, HIGH);  // Disable motors (optional, can also just stop pulses)
+    digitalWrite(RENA, HIGH);
+  }
 }
 
 ros::Subscriber<std_msgs::String> sub("motor_command", &commandCallback);
@@ -24,7 +32,6 @@ void setup() {
   nh.initNode();
   nh.subscribe(sub);
 
-  // Set pin modes
   pinMode(LPUL, OUTPUT);
   pinMode(LDIR, OUTPUT);
   pinMode(LENA, OUTPUT);
@@ -32,44 +39,22 @@ void setup() {
   pinMode(RDIR, OUTPUT);
   pinMode(RENA, OUTPUT);
 
-  // Enable the driver initially
-  digitalWrite(LENA, LOW);  // LOW to enable, HIGH to disable
-  digitalWrite(RENA, LOW);  // LOW to enable, HIGH to disable
+  digitalWrite(LENA, LOW);  // Enable by default
+  digitalWrite(RENA, LOW);
+  digitalWrite(LDIR, HIGH);  // Set direction (HIGH = Forward)
+  digitalWrite(RDIR, HIGH);
 }
 
 void loop() {
-  nh.spinOnce();  // Process incoming ROS messages
+  nh.spinOnce();  // Check for new commands
 
-  if (command == "F") {
-    // Re-enable motors when moving forward
-    digitalWrite(LENA, LOW);
-    digitalWrite(RENA, LOW);
-    moveForward();
-    command = "";  // Clear command after execution
-  } else if (command == "S") {
-    stopMotors();
-    command = "";  // Clear command after execution
-  }
-}
-
-void moveForward() {
-  digitalWrite(LDIR, HIGH);
-  digitalWrite(RDIR, HIGH);
-
-  for (int i = 0; i < 4000; i++) {  // Adjust steps for distance/speed
+  if (shouldMove) {
+    // Generate continuous pulses while shouldMove is true
     digitalWrite(LPUL, HIGH);
-    delayMicroseconds(10); // Adjust pulse width for speed
-    digitalWrite(LPUL, LOW);
-    delayMicroseconds(10);
     digitalWrite(RPUL, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(10);  // Controls speed (lower = faster)
+    digitalWrite(LPUL, LOW);
     digitalWrite(RPUL, LOW);
     delayMicroseconds(10);
   }
-}
-
-void stopMotors() {
-  // Disable motor driver to stop completely
-  digitalWrite(LENA, HIGH);  // HIGH to disable
-  digitalWrite(RENA, HIGH);  // HIGH to disable
 }
