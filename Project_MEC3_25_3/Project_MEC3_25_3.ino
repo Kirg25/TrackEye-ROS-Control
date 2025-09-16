c7303e0b5a2554940e56b1d145957ade2426879b
Kirg25 <inventorperish@gmail.com>
2025-09-16 09:42:25 +0800

    Update Project_MEC3_25_3.ino

--- Project_MEC3_25_3/Project_MEC3_25_3.ino
+++ Project_MEC3_25_3/Project_MEC3_25_3.ino
@@ -1,54 +1,33 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 
-// --- Stepper Motor Pins ---
-const int STEP_LINEAR = 10;
-const int DIR_LINEAR = 11;
+const int LPUL = 2;  // Left Pulse
+const int LDIR = 3;  // Left Direction
+const int LENA = 4;  // Left Enable
 
-// --- Drive Motor Pins  ---
-const int LPUL = 2;
-const int LDIR = 3;
-const int LENA = 4;
-
-const int RPUL = 5;
-const int RDIR = 6;
-const int RENA = 7;
+const int RPUL = 5;  // Right Pulse
+const int RDIR = 6;  // Right Direction
+const int RENA = 7;  // Right Enable
 
 ros::NodeHandle nh;
-
-// Linear actuator control
-volatile bool linearMoving = false;
-volatile bool linearDirection = true;  // true = extend, false = retract
-
-// Timing variables
-unsigned long moveStartMillis = 0;
-const unsigned long moveDuration = 5000;  // 5 seconds duration
-
-unsigned long lastStepMicros = 0;
-const unsigned int stepInterval = 200;  // Speed control (lower = faster)
+String command = "";  
+bool shouldMove = false;  
+bool reverseMode = false;  // NEW flag for reverse
 
 void commandCallback(const std_msgs::String &msg) {
-  String cmd = msg.data;
-
-  if (cmd == "EXTEND") {
-    digitalWrite(DIR_LINEAR, HIGH);
-    linearDirection = true;
-    linearMoving = true;
-    moveStartMillis = millis();
-  } else if (cmd == "RETRACT") {
-    digitalWrite(DIR_LINEAR, LOW);
-    linearDirection = false;
-    linearMoving = true;
-    moveStartMillis = millis();
-  } else if (cmd == "STOP_LINEAR") {
-    linearMoving = false;
-    digitalWrite(STEP_LINEAR, LOW);
-  } else if (cmd == "F") {
-    digitalWrite(LPUL, HIGH); digitalWrite(RPUL, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(LPUL, LOW); digitalWrite(RPUL, LOW);
-  } else if (cmd == "S") {
-    digitalWrite(LPUL, LOW); digitalWrite(RPUL, LOW);
+  command = msg.data;
+  if (command == "F") {
+    shouldMove = true;
+    reverseMode = false;
+    digitalWrite(LDIR, HIGH);  // Forward
+    digitalWrite(RDIR, HIGH);
+  } else if (command == "R") {
+    shouldMove = true;
+    reverseMode = true;
+    digitalWrite(LDIR, LOW);   // Reverse
+    digitalWrite(RDIR, LOW);
+  } else if (command == "S") {
+    shouldMove = false;
   }
 }
 
@@ -58,36 +37,28 @@ void setup() {
   nh.initNode();
   nh.subscribe(sub);
 
-  // Stepper motor setup
-  pinMode(STEP_LINEAR, OUTPUT);
-  pinMode(DIR_LINEAR, OUTPUT);
-  digitalWrite(STEP_LINEAR, LOW);
-  digitalWrite(DIR_LINEAR, LOW);
+  pinMode(LPUL, OUTPUT);
+  pinMode(LDIR, OUTPUT);
+  pinMode(LENA, OUTPUT);
+  pinMode(RPUL, OUTPUT);
+  pinMode(RDIR, OUTPUT);
+  pinMode(RENA, OUTPUT);
 
-  // Drive motor setup
-  pinMode(LPUL, OUTPUT); pinMode(LDIR, OUTPUT); pinMode(LENA, OUTPUT);
-  pinMode(RPUL, OUTPUT); pinMode(RDIR, OUTPUT); pinMode(RENA, OUTPUT);
-  digitalWrite(LENA, LOW); digitalWrite(RENA, LOW);
-  digitalWrite(LDIR, HIGH); digitalWrite(RDIR, HIGH);
+  digitalWrite(LENA, LOW);  // Enable motors
+  digitalWrite(RENA, LOW);
+  digitalWrite(LDIR, HIGH); // Default Forward
+  digitalWrite(RDIR, HIGH);
 }
 
 void loop() {
-  unsigned long nowMicros = micros();
-  unsigned long nowMillis = millis();
-
-  if (linearMoving && (nowMillis - moveStartMillis < moveDuration)) {
-    if (nowMicros - lastStepMicros >= stepInterval) {
-      digitalWrite(STEP_LINEAR, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(STEP_LINEAR, LOW);
-      lastStepMicros = nowMicros;
-    }
-  } else if (linearMoving) {
-    // Stop after 5 seconds
-    linearMoving = false;
-    digitalWrite(STEP_LINEAR, LOW);
-  }
-
   nh.spinOnce();
-  delay(1);
+
+  if (shouldMove) {
+    digitalWrite(LPUL, HIGH);
+    digitalWrite(RPUL, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(LPUL, LOW);
+    digitalWrite(RPUL, LOW);
+    delayMicroseconds(10);
+  }
 }
